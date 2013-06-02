452240962ffb012ae05f254592f0249c73f92f6a
Weston M. Binford III <wbinford@gmail.com>
2013-06-01 19:18:24 -0500

    Upgraded Sln to VS2010 & ref CommonAssemblyInfo

--- src/LinqToHtml/Properties/AssemblyInfo.cs
+++ src/LinqToHtml/Properties/AssemblyInfo.cs
@@ -6,13 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly:AssemblyTitle("LinqToHtml - http://github.com/mvba/LinqToHtml/ - MIT License")]
-[assembly:AssemblyDescription("")]
-[assembly:AssemblyConfiguration("")]
-[assembly:AssemblyCompany("McCreary, Veselka, Bragg & Allen, P.C.")]
-[assembly:AssemblyProduct("LinqToHtml")]
-[assembly:AssemblyCopyright("Copyright © 2009-2010 McCreary, Veselka, Bragg & Allen, P.C.")]
-[assembly:AssemblyTrademark("")]
-[assembly:AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -23,17 +16,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly:Guid("40849779-d306-417c-afba-0b749abd3c54")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly:AssemblyVersion("1.0.0.0")]
-[assembly:AssemblyFileVersion("1.0.0.0")]
